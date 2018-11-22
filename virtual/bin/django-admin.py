1e6d5b23edc35b651dd9407386ac0958963d9cfa
maxwellharon <maxwellharon54@gmail.com>
2018-11-22 03:48:14 +0300

    Online Commerce site

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/max/Documents/moringa-school-projects/gallery-master/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
